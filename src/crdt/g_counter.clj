547a3ea6091b854098e7bcca64f6e669628572c8
Ильдар Фатыхов <i.fatikhov@infotech.team>
2017-07-07 01:00:31 +0300

    added merge and value functions

--- src/crdt/g_counter.clj
+++ src/crdt/g_counter.clj
@@ -5,6 +5,14 @@
     (atom {node-id 0})))
 
 (defn increment-g-counter
-  [g-counter node-id value]
-  (let [increment-func (fn [current-val] (+ current-val value))]
+  [g-counter node-id value-to-add]
+  (let [increment-func (fn [current-val] (+ current-val value-to-add))]
     (swap! g-counter update-in [node-id] increment-func)))
+
+(defn merge-g-counter
+  [g-counter another-g-counter]
+  (atom (merge @g-counter @another-g-counter)))
+
+(defn value [g-counter]
+  (let [g-counter-state @g-counter]
+    (reduce + (vals g-counter-state))))
